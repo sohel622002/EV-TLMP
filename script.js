6ffcb5700ecf19ba74781a9ea3ca1dfcee812f99
sohel <sohelshaikh01231@gmail.com>
2023-12-23 15:52:39 +0530

    EV Project

---
+++ script.js
@@ -0,0 +1,22 @@
+const mainContent = document.querySelector(".main");
+const companyProfileContent = document
+  .querySelector("[data-company-profile-temp]")
+  .content.cloneNode(true)
+  .querySelector(".company-profile-wrapper");
+const productCatalogueContent = document
+  .querySelector("[data-catalogue-temp]")
+  .content.cloneNode(true)
+  .querySelector(".main-section");
+
+const companyProfileLink = document.querySelector(".company-profile-link");
+const productCatalogueLink = document.querySelector('.product-catalogue-link')
+
+companyProfileLink.addEventListener("click", () => {
+  mainContent.innerHTML = "";
+  mainContent.append(companyProfileContent);
+});
+
+productCatalogueLink.addEventListener("click", () => {
+    mainContent.innerHTML = "";
+    mainContent.append(productCatalogueContent);
+  });
