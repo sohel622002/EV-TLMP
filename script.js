5b5ae69005683b801317f00d6a55fda04ecf445b
sohel <sohelshaikh01231@gmail.com>
2023-12-24 12:58:11 +0530

    Responsive Done for 3 pages...

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mainContent = document.querySelector(".main");
+
 const companyProfileContent = document
   .querySelector("[data-company-profile-temp]")
   .content.cloneNode(true)
@@ -19,34 +20,75 @@ const tlmpContent = document
   .querySelector("[data-tlmp]")
   .content.cloneNode(true)
   .querySelector(".main-section");
+const homecontent = document
+  .querySelector("[data-home]")
+  .content.cloneNode(true)
+  .querySelector(".main-section");
 
+const logoLink = document.querySelector(".main-content-logo-image")
 const companyProfileLink = document.querySelector(".company-profile-link");
 const productCatalogueLink = document.querySelector(".product-catalogue-link");
 const registerLoginLink = document.querySelector(".register-link");
 const contactUsLink = document.querySelector(".contact-link");
 const tlmpLink = document.querySelector(".tlmp-link");
 
-companyProfileLink.addEventListener("click", () => {
+let mobileOpen = false;
+const mobileSidebarLinks = document.querySelector(
+  ".mobile-navigations-wrapper"
+);
+
+function changeMainData(data) {
   mainContent.innerHTML = "";
-  mainContent.append(companyProfileContent);
+  mainContent.append(data);
+}
+
+function closeMobileSidebar(){
+  mobileSidebarLinks.style.transform = "translateX(-102%)";
+}
+
+logoLink.addEventListener("click", () => {
+  changeMainData(homecontent)
+})
+
+companyProfileLink.addEventListener("click", () => {
+  changeMainData(companyProfileContent);
 });
 
 productCatalogueLink.addEventListener("click", () => {
-  mainContent.innerHTML = "";
-  mainContent.append(productCatalogueContent);
+  changeMainData(productCatalogueContent);
 });
 
 registerLoginLink.addEventListener("click", () => {
-  mainContent.innerHTML = "";
-  mainContent.append(registerLoginContent);
+  changeMainData(registerLoginContent);
 });
 
 contactUsLink.addEventListener("click", () => {
-  mainContent.innerHTML = "";
-  mainContent.append(contactUsContent);
+  changeMainData(contactUsContent);
 });
 
 tlmpLink.addEventListener("click", () => {
-  mainContent.innerHTML = "";
-  mainContent.append(tlmpContent);
+  changeMainData(tlmpContent);
+});
+
+document.querySelector(".mobile-toggle-btn").addEventListener("click", () => {
+  mobileSidebarLinks.style.transform = "translateX(0%)";
+});
+
+document.querySelector(".mobile-nav-header i").addEventListener("click", () => {
+  mobileSidebarLinks.style.transform = "translateX(-102%)";
+});
+
+document.querySelector(".home-mobile-link").addEventListener("click", () => {
+  changeMainData(homecontent);
+  closeMobileSidebar();
+});
+
+document.querySelector(".company-profile-mobile-link").addEventListener("click", () => {
+  changeMainData(companyProfileContent);
+  closeMobileSidebar();
+});
+
+document.querySelector(".product-catalogue-mobile-link").addEventListener("click", () => {
+  changeMainData(productCatalogueContent);
+  closeMobileSidebar();
 });
